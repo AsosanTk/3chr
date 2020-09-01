9f3c82527a79c7ea8601d4fa336012bcd179107a
Asosan <61866641+AsosanTk@users.noreply.github.com>
2020-09-02 07:51:50 +0900

    stopload()関数の修正

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,10 @@ jQuery(function ($) {
     $(window).on("load", function () {
         stopload();
     })
-    setTimeout('stopload()', 10000);
+    setTimeout(stopload(), 10000);
     function stopload() {
         $(".loading").delay(500).fadeOut(500);
+        $(".loading-tag").delay(500).fadeOut(500);
     }
 
 
