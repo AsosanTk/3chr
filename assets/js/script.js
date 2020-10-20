8f1c1689ef55946c24ee17e2c503b503cc8d2b55
kosei <ka72suiden@icloud.com>
2020-10-20 21:26:32 +0900

    軽微な修正

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,8 @@ $(window).on("load", function() {
     stopload();
     setTimeout(stopload(), 10000);
     function stopload() {
-        $(".loading").delay(500).fadeOut(500);
-        $(".loading-tag").delay(500).fadeOut(500);
+        $(".loading").fadeOut(500);
+        $(".loading-tag").fadeOut(500);
     }
 });
 
