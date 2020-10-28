f570c132ac9340019f86cdf4bc9ba8d38c03bfd5
kosei <ka72suiden@icloud.com>
2020-10-28 21:43:58 +0900

    CountTimerの設置

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,6 +27,33 @@ jQuery(function ($) {
         });
     }
     fadeInUp();
+    
+    
+    /*counttime*/
+    function CountDays() {
+        var starttime = new Date("October 30, 2020 09:30:00");
+        var nowtime = new Date();
+        var counttime = starttime.getTime() - nowtime.getTime();
+        var d = Math.floor(counttime/86400000);
+        counttime -= d*86400000;
+        var h = Math.floor(counttime/3600000);
+        counttime -= h*3600000;
+        var m = Math.floor(counttime/60000);
+        counttime -= m*60000;
+        var s = Math.floor(counttime/1000);
+        var dd = ('00' + d).slice(-2);
+        var hh = ('00' + h).slice(-2);
+        var mm = ('00' + m).slice(-2);
+        var ss = ('00' + s).slice(-2);
+        $('.countdown-day').text(dd);
+        $('.countdown-hour').text(hh);
+        $('.countdown-min').text(mm);
+        $('.countdown-sec').text(ss);
+        setInterval(CountDays, 1000);
+    }
+    CountDays();
+    
+    
 
     /*スムーズスクロール*/
     var smoothlink = $('a[href^="#"]');
